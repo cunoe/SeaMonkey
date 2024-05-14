72954477313583672f91b3608561ed26405ce8e1
Cunoe <cunkycheng@gmail.com>
2024-05-14 17:53:38 +0800

    添加感谢

--- pages/about.vue
+++ pages/about.vue
@@ -4,12 +4,33 @@
 <template>
   <div class="container mx-auto max-w-screen-md">
     <div class="card card-side bg-base-100 shadow-xl max-h-30">
-      <figure><img src="https://s3.cunoe.com/files/bee505407a8f3deb12a3326744d7ccfb.jpg"  alt=""/></figure>
+      <figure><img src="https://avatars.githubusercontent.com/u/90205430"  alt=""/></figure>
       <div class="card-body">
         <h1 class="card-title text-5xl">CUNOE</h1>
-        <p></p>
+        <p class="text-2xl font-bold text-gray-300">开发</p>
         <div class="card-actions justify-end">
-          <div><nuxt-link to="/" class="btn btn-ghost text-3xl text-gray-400 hover:text-white"><Icon name="uil:github" class="hover:text-white" />Github</nuxt-link></div>
+          <div><nuxt-link to="https://github.com/cunoe" target="_blank" class="btn btn-ghost text-3xl text-gray-400 hover:text-white"><Icon name="uil:github" class="hover:text-white" />Github</nuxt-link></div>
+        </div>
+      </div>
+    </div>
+    <div class="p-5"></div>
+    <div class="card card-side bg-base-100 shadow-xl max-h-30">
+      <figure><img src="https://avatars.githubusercontent.com/u/103585196"  alt=""/></figure>
+      <div class="card-body">
+        <h1 class="card-title text-5xl">SangonomiyaKoko</h1>
+        <p class="text-2xl font-bold text-gray-300">感谢数据支持~</p>
+        <div class="card-actions justify-end">
+          <div><nuxt-link to="https://github.com/SangonomiyaKoko" target="_blank" class="btn btn-ghost text-3xl text-gray-400 hover:text-white"><Icon name="uil:github" class="hover:text-white" />Github</nuxt-link></div>
+        </div>
+      </div>
+    </div>
+    <div class="p-5"></div>
+    <div class="card card-side bg-base-100 shadow-xl max-h-30">
+      <div class="card-body">
+        <h1 class="card-title text-5xl">感谢</h1>
+        <p class="text-2xl font-bold text-gray-300">YuYuko | 西行寺雨季 科长 男人们的定远号 海上最速暴毙传说</p>
+        <div class="card-actions justify-end">
+          <div><nuxt-link to="https://github.com/wows-yuyuko" target="_blank" class="btn btn-ghost text-3xl text-gray-400 hover:text-white"><Icon name="uil:github" class="hover:text-white" />Github</nuxt-link></div>
         </div>
       </div>
     </div>
